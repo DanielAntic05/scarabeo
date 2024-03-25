515e85268579f0438c699d2f5d45f534ac07d4f2
DanielAntic05 <9698108@itisrossi.vi.it>
2024-03-25 12:20:46 +0100

    modified

---
+++ scarabeo/Trie.cs
@@ -0,0 +1,74 @@
+using System;
+
+namespace Scarabeo
+{
+    class CTrie
+    {
+        static readonly int ALPHABET_SIZE = 26;
+
+        internal class CTrieNode
+        {
+            public CTrieNode[] Children { get; set; }
+            public bool IsEndOfWord     { get; set; }
+
+            public CTrieNode()
+            {
+                Children = new CTrieNode[ALPHABET_SIZE];
+                IsEndOfWord = false;
+            }
+        }    
+
+        public CTrieNode Root { get; private set; }
+
+
+        public void Insert(string key)
+        {
+            CTrieNode tmpNode = Root;
+
+            for (int level = 0; level < key.Length; level++)
+            {
+                int index = key[level] - 'a';
+
+                if (tmpNode.Children[index] == null)
+                    tmpNode.Children[index] = new CTrieNode();
+
+                tmpNode = tmpNode.Children[index];
+            }
+
+            tmpNode.IsEndOfWord = true;
+        }
+
+
+        public bool Search(string key)
+        {
+            CTrieNode tmpNode = Root;
+
+            for (int level = 0; level < key.Length; level++)
+            {
+                int index = key[level] - 'a';
+
+                if (tmpNode.Children[index] == null)
+                    return false;
+
+                tmpNode = tmpNode.Children[index];
+            }
+
+            return (tmpNode.IsEndOfWord);
+        }
+
+
+        public void Delete(string key)
+        {
+            CTrieNode tmpNode = Root;
+
+            for (int level = 0; level < key.Length; level++)
+            {
+                int index = key[level] - 'a';
+
+                // if (tmpNode.Children[index] == )
+            }
+        }
+    }
+
+    
+}
